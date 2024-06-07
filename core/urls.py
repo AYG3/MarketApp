3bb080e36fdee443c6c48011445189b5a7e7cd63
Ayoku Gilbert <gilbertayoku3@gmail.com>
2024-06-07 11:31:07 +0100

    progress

--- core/urls.py
+++ core/urls.py
@@ -1,7 +1,10 @@
 from django.urls import path
+from . import views
 
+# https://youtu.be/ZxMB6Njs3ck - creating user login
 app_name='core'
 
 urlpatterns = [
-    
+    path('', views.index),
+    path("contact/", views.contact, name="contact"),
 ]
