3b760a3e57a7bf709b53ad746a3789be5dede584
Ayoku Gilbert <gilbertayoku3@gmail.com>
2024-06-07 11:01:18 +0100

    progress

--- item/views.py
+++ item/views.py
@@ -7,6 +7,6 @@ from .models import Item
 # Create your views here.
 def detail(request, pk):
     item = get_object_or_404(Item, pk=pk)
-    related_items = Item.objects.filter(category=item.category, is_sold=False).exclude
+    related_items = Item.objects.filter(category=item.category, is_sold=False).exclude(pk=item.pk)
 
     return render(request, 'item/details.html', {'item':item, 'related_items':related_items})
